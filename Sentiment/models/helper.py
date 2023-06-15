98db65d1a0dda73ade7f8ad3f807adb4ae72263d
wideraHannes <61525214+wideraHannes@users.noreply.github.com>
2023-06-15 13:03:08 +0200

    Add models for Sentiment Analysis

---
+++ Sentiment/models/helper.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from sklearn.utils import shuffle
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir == "neg" else 1)
+    texts, labels= shuffle(texts, labels, random_state=42)
+    return texts, labels
