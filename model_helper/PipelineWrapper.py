608f0e61a47a6615d24bf6d0d919029a23a54ec5
wideraHannes <61525214+wideraHannes@users.noreply.github.com>
2023-06-22 13:52:10 +0200

    Added Model Helper

---
+++ model_helper/PipelineWrapper.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+
+class PipelineWrapper():
+    def __init__(self, model, vectorizer, corpus_test, corpus_train, y_test, y_train):
+        self.corpus_test = corpus_test
+        self.corpus_train = corpus_train
+        self.y_test = y_test
+        self.y_train = y_train
+        self.pipeline = Pipeline([('vectorizer', vectorizer), ('model', model)])
+
+
+    def fit(self):
+        self.pipeline.fit(self.corpus_train, self.y_train)
+        return self
+
+    def predict(self, X):
+        return self.pipeline.predict(X)
+    
+    def predict_proba(self, X):
+        return self.pipeline.predict_proba(X)
+    
+    def report(self):
+        report = classification_report(self.y_test, self.predict(self.corpus_test),  output_dict=True)
+        return pd.DataFrame(report)
+    
+    # named steps
+    def get_feature_names(self):
+        return self.pipeline.named_steps['vectorizer'].get_feature_names_out()
